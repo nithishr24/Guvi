b3058ffa1e834815ba1fce6ace2c15f50f536140
nithishr24 <113576968+nithishr24@users.noreply.github.com>
2023-11-26 13:44:45 +0530

    profile.php

--- php/profile.php
+++ php/profile.php
@@ -12,7 +12,7 @@ $database = $client->$databaseName;
 $collection = $database->$collectionName;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = "exampl@one.com";
+    $email = "example@one.com";
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
     $age = $_POST["age"];
